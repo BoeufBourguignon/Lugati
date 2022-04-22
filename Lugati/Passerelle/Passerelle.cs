13b4e9ca473a862f24edc8716c7ec1ffdc21e51f
Plofou <104132465+Plofou@users.noreply.github.com>
2022-04-22 11:33:05 +0200

    passerelle
    ajout passerelle classe hebergement

---
+++ Lugati/Passerelle/Passerelle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passerelle
+{
+    public static class Passerelle
+    {
+        private static SqlConnection connexionBaseTravel = new SqlConnection(
+            "Data Source=DESKTOP-1QBGM1M;" +
+            "Initial Catalog=base_lugati;" +
+            "User Id=LugatiApp;" +
+            "Password=b4n4n3");
+    }
+}
