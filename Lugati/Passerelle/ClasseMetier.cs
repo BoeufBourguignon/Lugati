923a118d5313e9260644d018da95ea868490b77f
Plofou <104132465+Plofou@users.noreply.github.com>
2022-04-22 11:40:39 +0200

    commit

--- Lugati/Passerelle/ClasseMetier.cs
+++ Lugati/Passerelle/ClasseMetier.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Passerelle
 {
-    internal class ClasseMetier
+    public class Hebergement
     {
+        public int idHebergement, nbEtoiles, prix;
+        public string nomHebergement, adresse, ville, CP, tel;
+
+        public Hebergement(int idHebergement, string nomHebergement, string adresse, string ville, string CP, string tel, int nbEtoiles, int prix)
+        {
+            this.idHebergement = idHebergement;
+            this.nomHebergement = nomHebergement;
+            this.adresse = adresse;
+            this.ville = ville;
+            this.CP = CP;
+            this.tel = tel;
+            this.nbEtoiles = nbEtoiles;
+            this.prix = prix;
+        }
     }
 }
--- Lugati/Passerelle/Passerelle.cs
+++ Lugati/Passerelle/Passerelle.cs
@@ -14,5 +14,44 @@ namespace Passerelle
             "Initial Catalog=base_lugati;" +
             "User Id=LugatiApp;" +
             "Password=b4n4n3");
+
+        /// <summary>
+        /// Retourne tous les hotels présent dans la base de données
+        /// </summary>
+        /// <returns>Collection d'hotels</returns>
+        public static List<hebergement> GetLesVoyages()
+        {
+            List<Voyage> lesVoyages = new List<Voyage>();
+
+            SqlCommand reqLesVoyages =
+                new SqlCommand("SELECT num, titre, nbPlace, tarif " +
+                                "FROM Voyage",
+                ClassePasserelle.connexionBaseTravel);
+
+            ClassePasserelle.connexionBaseTravel.Open();
+
+            SqlDataReader readerLesVoyages = reqLesVoyages.ExecuteReader();
+
+            if (readerLesVoyages.HasRows)
+            {
+                while (readerLesVoyages.Read())
+                {
+                    lesVoyages.Add(new Voyage(
+                            (int)readerLesVoyages[0],
+                            readerLesVoyages[1].ToString(),
+                            (int)readerLesVoyages[2],
+                            (int)readerLesVoyages[3])
+                        );
+                }
+            }
+            else
+            {
+                throw new Exception("Il n'existe aucun voyage");
+            }
+
+            ClassePasserelle.connexionBaseTravel.Close();
+
+            return lesVoyages;
+        }
     }
 }
