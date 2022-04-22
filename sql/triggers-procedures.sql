1fbd5be236d18feece9b806492ec1584d2a2fe46
Arcen3466 <gabriel.boigsimon@gmail.com>
2022-04-22 08:36:48 +0200

    Update triggers-procedures.sql

--- sql/triggers-procedures.sql
+++ sql/triggers-procedures.sql
@@ -1,17 +1,32 @@
--- Quand on ajoute une participation (insert dans participer) (activité)
--- On vérifie que l'id de l'activité n'est pas à une date/heure d'une autre activité/session
+-- TRIGGERS
+
+-- Quand on ajoute une participation (insert dans participer) (activitï¿½)
+-- On vï¿½rifie que l'id de l'activitï¿½ n'est pas ï¿½ une date/heure d'une autre activitï¿½/session
 -- -> (select concat(date, ' ', heure) from activite where numActivite = inserted.numActivite) not in (select concat(date, ' ', heure) from activite union select concat(date, ' ', heure) from session)
 
 
-(select concat(date, ' ', heure) from activite where numActivite = inserted.numActivite) not in
+--(select concat(date, ' ', heure) from activite where numActivite = inserted.numActivite) not in
 
-select concat(date, ' ', heure) from activite
-union
-select concat(date, ' ', heure) from session;
+--select concat(date, ' ', heure) from activite
+--union
+--select concat(date, ' ', heure) from session;
 
 go
-create trigger ti_participer on participer instead of insert
+create trigger ti_participer on participer 
+instead of insert
 as
 begin
+	declare cursParticiper
+	for
+		select idParticipant, numActivite
+		from inserted 
+	declare @idParticipant int, @numActivite int
+	fetch next from cursParticiper into @idParticipant, @numActivite
+	while(@@FETCH_STATUS = 0)
+	begin
+		declare @dateActivite varchar(20)
 
-end;
+		fetch next from cursParticiper into @idParticipant, @numActivite
+	end
+end
+go
