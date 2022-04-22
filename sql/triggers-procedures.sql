431e69d21bb99dd765ff095fec1a3c81c1ccc9a1
T.Leclere <thibaud.leclere@gmail.com>
2022-04-22 09:28:01 +0200

    premiers triggers

--- sql/triggers-procedures.sql
+++ sql/triggers-procedures.sql
@@ -11,12 +11,12 @@
 --union
 --select concat(date, ' ', heure) from session;
 
-go
-create trigger ti_participer on participer 
-instead of insert
+
+create trigger tiu_participer on participer 
+after insert, update
 as
 begin
-	declare cursParticiper
+	declare cursParticiper cursor
 	for
 		select idParticipant, numActivite
 		from inserted 
@@ -24,9 +24,56 @@ begin
 	fetch next from cursParticiper into @idParticipant, @numActivite
 	while(@@FETCH_STATUS = 0)
 	begin
-		declare @dateActivite varchar(20)
+		declare @dateActivite varchar(30)
+		set @dateActivite = (
+			select concat(date, ' ', heure) 
+			from activite 
+			where numActivite = @numActivite
+		)
+
+		if(@dateActivite IN (
+				select concat(date, ' ', heure) from activite a join participer p on a.numActivite = p.numActivite and p.idParticipant = @idParticipant
+				union
+				select concat(date, ' ', heure) from session s join inscrire i on i.numSession = s.numSession and i.idParticipant = @idParticipant)
+			)
+			throw 50001, 'Une personne ne peut pas participer à deux sessions ou activités en même temps', 0
+		else 
+			insert into participer (idParticipant, numActivite) values (@idParticipant, @numActivite)
 
 		fetch next from cursParticiper into @idParticipant, @numActivite
+	end
+end
+go
+
+create trigger tiu_inscrire on inscrire 
+after insert, update
+as
+begin
+	declare cursParticiper cursor
+	for
+		select idParticipant, numSession
+		from inserted 
+	declare @idParticipant int, @numSession int
+	fetch next from cursParticiper into @idParticipant, @numSession
+	while(@@FETCH_STATUS = 0)
+	begin
+		declare @dateSession varchar(30)
+		set @dateSession = (
+			select concat(date, ' ', heure) 
+			from session 
+			where numSession = @numSession
+		)
+
+		if(@dateSession IN (
+				select concat(date, ' ', heure) from activite a join participer p on a.numActivite = p.numActivite and p.idParticipant = @idParticipant
+				union
+				select concat(date, ' ', heure) from session s join inscrire i on i.numSession = s.numSession and i.idParticipant = @idParticipant)
+			)
+			throw 50001, 'Une personne ne peut pas participer à deux sessions ou activités en même temps', 0
+		else 
+			insert into inscrire(idParticipant, numSession) values (@idParticipant, @numSession)
+
+		fetch next from cursParticiper into @idParticipant, @numSession
 	end
 end
 go
