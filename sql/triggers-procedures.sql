c46b65f89a8971dc99b0c761ea6398a5a71b4dec
T.Leclere <thibaud.leclere@gmail.com>
2022-04-22 09:29:25 +0200

    premiers triggers

--- sql/triggers-procedures.sql
+++ sql/triggers-procedures.sql
@@ -1,17 +1,4 @@
 -- TRIGGERS
-
--- Quand on ajoute une participation (insert dans participer) (activit�)
--- On v�rifie que l'id de l'activit� n'est pas � une date/heure d'une autre activit�/session
--- -> (select concat(date, ' ', heure) from activite where numActivite = inserted.numActivite) not in (select concat(date, ' ', heure) from activite union select concat(date, ' ', heure) from session)
-
-
---(select concat(date, ' ', heure) from activite where numActivite = inserted.numActivite) not in
-
---select concat(date, ' ', heure) from activite
---union
---select concat(date, ' ', heure) from session;
-
-
 create trigger tiu_participer on participer 
 after insert, update
 as
