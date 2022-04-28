a8acf4b84b0e5607d3be9d824a3471a6d9c44564
BoeufBourguignon <thibaud.leclere@gmail.com>
2022-04-28 12:07:01 +0200

    ajouts classe métier

--- Lugati/Passerelle/ClasseMetier.cs
+++ Lugati/Passerelle/ClasseMetier.cs
@@ -27,6 +27,7 @@ namespace Passerelle
             this.prix = prix;
         }
     }
+
     /// <summary>
     /// Class
     /// <c>Session</c>
@@ -48,6 +49,66 @@ namespace Passerelle
         }
     }
 
+    /// <summary>
+    /// Class
+    /// <c>Activite</c>
+    /// </summary>
+    public class Activite
+    {
+        public int numActivite { get; set; }
+        public int tarif { get; set; }
+        public int nbPlace { get; set; }
+        public string libelle { get; set; }
+        public DateTime heure { get; set; }
+        public DateTime date { get; set; }
 
+        public Activite() { }
 
+        public Activite(int numActivite, string libelle, int tarif, int nbPlace, DateTime date, DateTime heure)
+        {
+            this.numActivite = numActivite;
+            this.libelle = libelle;
+            this.tarif = tarif;
+            this.nbPlace = nbPlace;
+            this.date = date;
+            this.heure = heure;
+        }
+    }
+
+    /// <summary>
+    /// Class
+    /// <c>Participant</c>
+    /// </summary>
+    public class Participant
+    {
+        public int idParticipant { get; set; }
+        public string nom { get; set; }
+        public string prenom { get; set; }
+        public char genre { get; set; }
+        public string adresse { get; set; }
+        public string ville { get; set; }
+        public string cp { get; set; }
+        public Hebergement hebergement { get; set; }
+        public Ligue ligue { get; set; }
+
+        public Participant() { }
+
+        public Participant(int idParticipant, string nom, string prenom, char genre, string adresse, string ville, string cp, Hebergement hebergement, Ligue ligue)
+        {
+            this.idParticipant = idParticipant;
+            this.nom = nom;
+            this.prenom = prenom;
+            this.genre = genre;
+            this.adresse = adresse;
+            this.ville = ville;
+            this.cp = cp;
+            this.hebergement = hebergement;
+            this.ligue = ligue;
+        }
+    }
+
+    public class Ligue
+    {
+        //TODO (Gaby)
+    }
 }
