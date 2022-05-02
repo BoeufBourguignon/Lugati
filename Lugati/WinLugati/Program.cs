8f375b1f110b93c7f6b9c484b1e940d7ee56f065
t.leclere <thibaud.leclere@gmail.com>
2022-05-02 12:29:00 +0200

    commit (c'est le titre)

--- Lugati/WinLugati/Program.cs
+++ Lugati/WinLugati/Program.cs
@@ -13,7 +13,7 @@ namespace WinLugati
         /// </summary>
         [STAThread]
         static void Main()
-        {
+        {aa
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmHebergement());
