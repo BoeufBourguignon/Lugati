685ea4ee6495d1c4a9a4726d2cba6e45a3cde913
BoeufBourguignon <thibaud.leclere@gmail.com>
2022-05-13 10:51:19 +0200

    changement menu

--- Lugati/WinLugati/FrmMenu.Designer.cs
+++ Lugati/WinLugati/FrmMenu.Designer.cs
@@ -28,103 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.menuStrip = new System.Windows.Forms.MenuStrip();
-            this.frmParticipantToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.participantToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.frmHebergementToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.hebergementToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.frmSessionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.sessionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.frmActiviteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.activiteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuStrip.SuspendLayout();
+            this.menuLugati = new System.Windows.Forms.MenuStrip();
+            this.menuLugatiItemParticipant = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuLugatiItemHebergement = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuLugatiItemSession = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuLugatiItemActivite = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuLugati.SuspendLayout();
             this.SuspendLayout();
             // 
-            // menuStrip
+            // menuLugati
             // 
-            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.frmParticipantToolStripMenuItem,
-            this.frmHebergementToolStripMenuItem,
-            this.frmSessionToolStripMenuItem,
-            this.frmActiviteToolStripMenuItem});
-            this.menuStrip.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip.Name = "menuStrip";
-            this.menuStrip.Size = new System.Drawing.Size(800, 24);
-            this.menuStrip.TabIndex = 0;
-            this.menuStrip.Text = "menuStrip1";
+            this.menuLugati.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuLugatiItemParticipant,
+            this.menuLugatiItemHebergement,
+            this.menuLugatiItemSession,
+            this.menuLugatiItemActivite});
+            this.menuLugati.Location = new System.Drawing.Point(0, 0);
+            this.menuLugati.Name = "menuLugati";
+            this.menuLugati.Size = new System.Drawing.Size(800, 24);
+            this.menuLugati.TabIndex = 0;
+            this.menuLugati.Text = "menuStrip1";
             // 
-            // frmParticipantToolStripMenuItem
+            // menuLugatiItemParticipant
             // 
-            this.frmParticipantToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.participantToolStripMenuItem});
-            this.frmParticipantToolStripMenuItem.Name = "frmParticipantToolStripMenuItem";
-            this.frmParticipantToolStripMenuItem.Size = new System.Drawing.Size(97, 20);
-            this.frmParticipantToolStripMenuItem.Text = "FrmParticipant";
+            this.menuLugatiItemParticipant.Name = "menuLugatiItemParticipant";
+            this.menuLugatiItemParticipant.Size = new System.Drawing.Size(81, 20);
+            this.menuLugatiItemParticipant.Text = "Participants";
+            this.menuLugatiItemParticipant.Click += new System.EventHandler(this.menuLugatiItemParticipant_Click);
             // 
-            // participantToolStripMenuItem
+            // menuLugatiItemHebergement
             // 
-            this.participantToolStripMenuItem.Name = "participantToolStripMenuItem";
-            this.participantToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
-            this.participantToolStripMenuItem.Text = "Participant";
-            this.participantToolStripMenuItem.Click += new System.EventHandler(this.participantToolStripMenuItem_Click);
+            this.menuLugatiItemHebergement.Name = "menuLugatiItemHebergement";
+            this.menuLugatiItemHebergement.Size = new System.Drawing.Size(92, 20);
+            this.menuLugatiItemHebergement.Text = "Hebergement";
+            this.menuLugatiItemHebergement.Click += new System.EventHandler(this.frmHebergementToolStripMenuItem_Click);
             // 
-            // frmHebergementToolStripMenuItem
+            // menuLugatiItemSession
             // 
-            this.frmHebergementToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.hebergementToolStripMenuItem});
-            this.frmHebergementToolStripMenuItem.Name = "frmHebergementToolStripMenuItem";
-            this.frmHebergementToolStripMenuItem.Size = new System.Drawing.Size(113, 20);
-            this.frmHebergementToolStripMenuItem.Text = "FrmHebergement";
+            this.menuLugatiItemSession.Name = "menuLugatiItemSession";
+            this.menuLugatiItemSession.Size = new System.Drawing.Size(58, 20);
+            this.menuLugatiItemSession.Text = "Session";
+            this.menuLugatiItemSession.Click += new System.EventHandler(this.frmSessionToolStripMenuItem_Click);
             // 
-            // hebergementToolStripMenuItem
+            // menuLugatiItemActivite
             // 
-            this.hebergementToolStripMenuItem.Name = "hebergementToolStripMenuItem";
-            this.hebergementToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
-            this.hebergementToolStripMenuItem.Text = "Hebergement";
-            this.hebergementToolStripMenuItem.Click += new System.EventHandler(this.hebergementToolStripMenuItem_Click);
-            // 
-            // frmSessionToolStripMenuItem
-            // 
-            this.frmSessionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.sessionToolStripMenuItem});
-            this.frmSessionToolStripMenuItem.Name = "frmSessionToolStripMenuItem";
-            this.frmSessionToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
-            this.frmSessionToolStripMenuItem.Text = "FrmSession";
-            // 
-            // sessionToolStripMenuItem
-            // 
-            this.sessionToolStripMenuItem.Name = "sessionToolStripMenuItem";
-            this.sessionToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
-            this.sessionToolStripMenuItem.Text = "Session";
-            this.sessionToolStripMenuItem.Click += new System.EventHandler(this.sessionToolStripMenuItem_Click);
-            // 
-            // frmActiviteToolStripMenuItem
-            // 
-            this.frmActiviteToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.activiteToolStripMenuItem});
-            this.frmActiviteToolStripMenuItem.Name = "frmActiviteToolStripMenuItem";
-            this.frmActiviteToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
-            this.frmActiviteToolStripMenuItem.Text = "FrmActivite";
-            // 
-            // activiteToolStripMenuItem
-            // 
-            this.activiteToolStripMenuItem.Name = "activiteToolStripMenuItem";
-            this.activiteToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
-            this.activiteToolStripMenuItem.Text = "Activite";
-            this.activiteToolStripMenuItem.Click += new System.EventHandler(this.activiteToolStripMenuItem_Click);
+            this.menuLugatiItemActivite.Name = "menuLugatiItemActivite";
+            this.menuLugatiItemActivite.Size = new System.Drawing.Size(59, 20);
+            this.menuLugatiItemActivite.Text = "Activite";
             // 
             // FrmMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.menuStrip);
+            this.Controls.Add(this.menuLugati);
             this.IsMdiContainer = true;
-            this.MainMenuStrip = this.menuStrip;
+            this.MainMenuStrip = this.menuLugati;
             this.Name = "FrmMenu";
             this.Text = "FrmMenu";
-            this.menuStrip.ResumeLayout(false);
-            this.menuStrip.PerformLayout();
+            this.menuLugati.ResumeLayout(false);
+            this.menuLugati.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -132,14 +95,10 @@
 
         #endregion
 
-        private System.Windows.Forms.MenuStrip menuStrip;
-        private System.Windows.Forms.ToolStripMenuItem frmParticipantToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem participantToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem frmHebergementToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem hebergementToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem frmSessionToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem sessionToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem frmActiviteToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem activiteToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuLugati;
+        private System.Windows.Forms.ToolStripMenuItem menuLugatiItemParticipant;
+        private System.Windows.Forms.ToolStripMenuItem menuLugatiItemHebergement;
+        private System.Windows.Forms.ToolStripMenuItem menuLugatiItemSession;
+        private System.Windows.Forms.ToolStripMenuItem menuLugatiItemActivite;
     }
 }
--- Lugati/WinLugati/FrmMenu.cs
+++ Lugati/WinLugati/FrmMenu.cs
@@ -19,7 +19,7 @@ namespace WinLugati
             InitializeComponent();
         }
 
-        private void participantToolStripMenuItem_Click(object sender, EventArgs e)
+        private void menuLugatiItemParticipant_Click(object sender, EventArgs e)
         {
             if (Application.OpenForms["FrmParticipant"] == null)
             {
@@ -34,9 +34,9 @@ namespace WinLugati
             }
         }
 
-        private void hebergementToolStripMenuItem_Click(object sender, EventArgs e)
+        private void frmHebergementToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if(Application.OpenForms["FrmHebergement"] == null)
+            if (Application.OpenForms["FrmHebergement"] == null)
             {
                 FrmHebergement frmH = new FrmHebergement();
                 frmH.MdiParent = this;
@@ -49,7 +49,7 @@ namespace WinLugati
             }
         }
 
-        private void sessionToolStripMenuItem_Click(object sender, EventArgs e)
+        private void frmSessionToolStripMenuItem_Click(object sender, EventArgs e)
         {
             if (Application.OpenForms["FrmSession"] == null)
             {
@@ -60,21 +60,6 @@ namespace WinLugati
             else
             {
                 Application.OpenForms["FrmSession"].BringToFront();
-
-            }
-        }
-
-        private void activiteToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            if (Application.OpenForms["FrmActivite"] == null)
-            {
-                FrmActivite frmA = new FrmActivite();
-                frmA.MdiParent = this;
-                frmA.Show();
-            }
-            else
-            {
-                Application.OpenForms["FrmActivite"].BringToFront();
 
             }
         }
